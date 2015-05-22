67e2e14304cce03299234f650cadd5bbef249722
houleitaidii <ray1860302@gmail.com>
2015-05-22 15:55:09 +0800

    add code: paging

--- app/build/box.js
+++ app/build/box.js
@@ -1,36 +1,68 @@
-var JiraBox = React.createClass({displayName: "JiraBox",
-	getInitialState: function(){
-		return {data: []};
-	},
-	loadDataFromServer: function() {
-		var api = restful("54.213.55.78").port(3000);
-		var pulls = api.all("pulls");
-		var box = this;
-		var pullEntity_array = [];
-		pulls.getAll({page:1}).then(function(response){
-		  var pullEntity = response.body();
-		  for(var i=0;i<pullEntity.length;i++){
-			  var pull_data = pullEntity[i].data();
-			  pullEntity_array.push(pull_data);
-//			  if(i>10){
-//			  break;}
-		  };
-		  box.setState({data:pullEntity_array});
-		});
-	},
-	componentDidMount: function(){
-		this.loadDataFromServer();
-//		setInterval(this.loadDataFromServer, this.props.pollInterval);
-		$(".progress").hide();
+var LoadMore = React.createClass({displayName: "LoadMore",
+	handleClick: function(event){
+		var page = this.refs.page.getDOMNode().value.trim();
+		var page = parseInt(page);
+		page = page + 1;
+		this.refs.page.getDOMNode().value = page;
+		boxhanlderLoadmore = this.props.onLoadmoreClick;
+		boxhanlderLoadmore(this);
 	},
 	render: function(){
 		return (
-			React.createElement("div", {className: "box"}, 
-				React.createElement(JiraList, {data: this.state.data})
+			React.createElement("div", null, 
+				React.createElement("input", {type: "button", name: "loadmore", value: this.props.page, ref: "page", onClick: this.handleClick})
 			)
 		);
 	}
 });
+
+
+var JiraBox = React.createClass({displayName: "JiraBox",
+	getInitialState: function(){
+		return {data: [], incr_data:[], page: 1};
+	},
+	loadDataFromServer: function() {
+		$(".progress").show();
+		var api = restful("54.213.55.78").port(3000);
+		var pulls = api.all("pulls");
+		var box = this;
+		var pullEntity_array = [];
+		pulls.getAll({page:box.state.page}).then(function(response){
+		  var pullEntity = response.body();
+		  for(var i=0;i<pullEntity.length;i++){
+			  var pull_data = pullEntity[i].data();
+			  pullEntity_array.push(pull_data);
+		  };
+		  box.setState({incr_data:pullEntity_array});
+		  if(box.state.data.length == 0){
+		  	box.setState({data:pullEntity_array});
+		  }else{
+			var incr_data = box.state.incr_data;
+			var origin_data = box.state.data; 
+			var incred_data = origin_data.concat(incr_data);
+		    box.setState({data: incred_data});
+		    }
+		  $(".progress").hide();
+		});
+	},
+	componentDidMount: function(){
+		this.loadDataFromServer();
+	},
+	handleLoadmoreClick:function(loadmore){
+		var page_number = loadmore.refs.page.getDOMNode().value;
+		this.state.page = page_number;
+		this.loadDataFromServer();
+	},
+	render: function(){
+		return (
+			React.createElement("div", {className: "box"}, 
+				React.createElement(JiraList, {data: this.state.data}), 
+				React.createElement(LoadMore, {page: this.state.page, onLoadmoreClick: this.handleLoadmoreClick})
+			)			
+		);
+	}
+});
+
 
 React.render(
 	React.createElement(JiraBox, null),
--- app/src/box.js
+++ app/src/box.js
@@ -1,36 +1,68 @@
+var LoadMore = React.createClass({
+	handleClick: function(event){
+		var page = this.refs.page.getDOMNode().value.trim();
+		var page = parseInt(page);
+		page = page + 1;
+		this.refs.page.getDOMNode().value = page;
+		boxhanlderLoadmore = this.props.onLoadmoreClick;
+		boxhanlderLoadmore(this);
+	},
+	render: function(){
+		return (
+			<div>
+				<input type="button" name="loadmore" value={this.props.page} ref="page" onClick={this.handleClick} />
+			</div>
+		);
+	}
+});
+
+
 var JiraBox = React.createClass({
 	getInitialState: function(){
-		return {data: []};
+		return {data: [], incr_data:[], page: 1};
 	},
 	loadDataFromServer: function() {
+		$(".progress").show();
 		var api = restful("54.213.55.78").port(3000);
 		var pulls = api.all("pulls");
 		var box = this;
 		var pullEntity_array = [];
-		pulls.getAll({page:1}).then(function(response){
+		pulls.getAll({page:box.state.page}).then(function(response){
 		  var pullEntity = response.body();
 		  for(var i=0;i<pullEntity.length;i++){
 			  var pull_data = pullEntity[i].data();
 			  pullEntity_array.push(pull_data);
-//			  if(i>10){
-//			  break;}
 		  };
-		  box.setState({data:pullEntity_array});
+		  box.setState({incr_data:pullEntity_array});
+		  if(box.state.data.length == 0){
+		  	box.setState({data:pullEntity_array});
+		  }else{
+			var incr_data = box.state.incr_data;
+			var origin_data = box.state.data; 
+			var incred_data = origin_data.concat(incr_data);
+		    box.setState({data: incred_data});
+		    }
+		  $(".progress").hide();
 		});
 	},
 	componentDidMount: function(){
 		this.loadDataFromServer();
-//		setInterval(this.loadDataFromServer, this.props.pollInterval);
-		$(".progress").hide();
+	},
+	handleLoadmoreClick:function(loadmore){
+		var page_number = loadmore.refs.page.getDOMNode().value;
+		this.state.page = page_number;
+		this.loadDataFromServer();
 	},
 	render: function(){
 		return (
 			<div className="box">
 				<JiraList data={this.state.data}/>
-			</div>
+				<LoadMore page={this.state.page} onLoadmoreClick={this.handleLoadmoreClick}/>
+			</div>			
 		);
 	}
 });
+
 
 React.render(
 	<JiraBox />,
